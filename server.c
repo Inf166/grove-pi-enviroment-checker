21db77fc8c179826e94c87b5570918d6660d31fc
Joel Mai <joel.maximilian@gmail.com>
2018-04-29 17:28:59 +0200

    Aufgabe 1.2 (Almost)

--- server.c
+++ server.c
@@ -1,143 +1,38 @@
-//
-// Created by Joel Mai on 26.04.2018.
-//
-/**************************************************************************/
-/* Application creates an iterative server design                         */
-/**************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <sys/types.h>
 #include <sys/socket.h>
+
 #include <netinet/in.h>
 
-#define SERVER_PORT  12345
+int main(){
 
-main (int argc, char *argv[])
-{
-    int    i, len, num, rc, on = 1;
-    int    listen_sd, accept_sd;
-    char   buffer[80];
-    struct sockaddr_in6   addr;
+    chat server_message[256] = "Du hast den Server erreicht";
 
-    /*************************************************/
-    /* If an argument was specified, use it to       */
-    /* control the number of incoming connections    */
-    /*************************************************/
-    if (argc >= 2)
-        num = atoi(argv[1]);
-    else
-        num = 1;
+    //Erstelle Server Socket
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    /*************************************************/
-    /* Create an AF_INET6 stream socket to receive   */
-    /* incoming connections on                       */
-    /*************************************************/
-    listen_sd = socket(AF_INET6, SOCK_STREAM, 0);
-    if (listen_sd < 0)
-    {
-        perror("socket() failed");
-        exit(-1);
-    }
+    //Definiere Server Adress-Familie
+    struct sockaddr_in server_ad;
+    serv_ad.sin_family = AF_INET;           //Adress-Familie
+    serv_ad.sin_port = htons(5678);         //Portnummer
+    serv_ad.sin_addr.s_addr = INADDR_ANY;   //Vlt noch ändern in IP Adresse
 
-    /*************************************************/
-    /* Allow socket descriptor to be reuseable       */
-    /*************************************************/
-    rc = setsockopt(listen_sd,
-                    SOL_SOCKET,  SO_REUSEADDR,
-                    (char *)&on, sizeof(on));
-    if (rc < 0)
-    {
-        perror("setsockopt() failed");
-        close(listen_sd);
-        exit(-1);
-    }
+    //bind den socket zu unser IP und port
+    bind(network_socket, (struct sockaddr *) &serv_ad, sizeof(serv_ad));
 
-    /*************************************************/
-    /* Bind the socket                               */
-    /*************************************************/
-    memset(&addr, 0, sizeof(addr));
-    addr.sin6_family      = AF_INET6;
-    memcpy(&addr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
-    addr.sin6_port        = htons(SERVER_PORT);
-    rc = bind(listen_sd,
-              (struct sockaddr *)&addr, sizeof(addr));
-    if (rc < 0)
-    {
-        perror("bind() failed");
-        close(listen_sd);
-        exit(-1);
-    }
+    //listen to conections
+    listen(server_socket, 5);
 
-    /*************************************************/
-    /* Set the listen back log                       */
-    /*************************************************/
-    rc = listen(listen_sd, 5);
-    if (rc < 0)
-    {
-        perror("listen() failed");
-        close(listen_sd);
-        exit(-1);
-    }
+    int client_socket;
 
-    /*************************************************/
-    /* Inform the user that the server is ready      */
-    /*************************************************/
-    printf("The server is ready\n");
-
-    /*************************************************/
-    /* Go through the loop once for each connection  */
-    /*************************************************/
-    for (i=0; i < num; i++)
-    {
-        /**********************************************/
-        /* Wait for an incoming connection            */
-        /**********************************************/
-        printf("Interation: %d\n", i+1);
-        printf("  waiting on accept()\n");
-        accept_sd = accept(listen_sd, NULL, NULL);
-        if (accept_sd < 0)
-        {
-            perror("accept() failed");
-            close(listen_sd);
-            exit(-1);
-        }
-        printf("  accept completed successfully\n");
-
-        /**********************************************/
-        /* Receive a message from the client          */
-        /**********************************************/
-        printf("  wait for client to send us a message\n");
-        rc = recv(accept_sd, buffer, sizeof(buffer), 0);
-        if (rc <= 0)
-        {
-            perror("recv() failed");
-            close(listen_sd);
-            close(accept_sd);
-            exit(-1);
-        }
-        printf("  <%s>\n", buffer);
-
-        /**********************************************/
-        /* Echo the data back to the client           */
-        /**********************************************/
-        printf("  echo it back\n");
-        len = rc;
-        rc = send(accept_sd, buffer, len, 0);
-        if (rc <= 0)
-        {
-            perror("send() failed");
-            close(listen_sd);
-            close(accept_sd);
-            exit(-1);
-        }
-
-        /**********************************************/
-        /* Close down the incoming connection         */
-        /**********************************************/
-        close(accept_sd);
-    }
-
-    /*************************************************/
-    /* Close down the listen socket                  */
-    /*************************************************/
-    close(listen_sd);
+    //accept Funktion      Server Socket    IP des Clients      Size of IP
+    client_socket = accept(server_socket, NULL, NULL);
+    //Send Funktion     Clientsocket, definierte Nachricht und Size of der Nachricht
+    send(client_socket, server_message, sizeof(server_message), 0);
+    //close Funktion
+    close(server_socket);
+    return 0;
 }
